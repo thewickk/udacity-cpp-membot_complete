e96f8f513c38f04988356750f7058c15d1b7f99a
Chad Lewis <chadlewis53@gmail.com>
2020-02-05 06:07:56 -0800

    Initial commit for first attempt at passing project requirements

---
+++ src/chatbot.h
@@ -0,0 +1,68 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+#include <iostream>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    
+    // TASK 2: The Rule of Five:
+    // 1. The Destructor
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+
+    // 2. The Copy Constructor
+    ChatBot(const ChatBot &source);
+
+    // 3. The Overloaded Copy Assignment Operator
+    ChatBot &operator=(const ChatBot &source);
+
+    // 4. The Move Constructor
+    ChatBot(ChatBot &&source);
+
+    // 5. The Overloaded Move Assignment Operator
+    ChatBot &operator=(ChatBot &&source);
+
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) 
+    { 
+        _rootNode = rootNode; 
+    }
+    void SetChatLogicHandle(ChatLogic *chatLogic) 
+    { 
+        _chatLogic = chatLogic; 
+    }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
